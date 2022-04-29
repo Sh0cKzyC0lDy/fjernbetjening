2da63764fb825636ccf63a8a70918113e6374750
Sh0cKzyC0lDy <72610271+Sh0cKzyC0lDy@users.noreply.github.com>
2022-04-29 22:11:21 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 - C5 - C5 - C5 - ", 120)
+    music.playMelody("C5 C5 C5 C5 - - - - ", 500)
+    music.playTone(988, music.beat(BeatFraction.Breve))
+    music.stopAllSounds()
+})
 radio.setGroup(1)
 basic.forever(function () {
     if (input.rotation(Rotation.Pitch) < -20) {
